996f27c2cc8e9892f97465bf10a809d0e53249cc
betocostadev <robertomarx@gmail.com>
2020-06-02 12:09:42 -0300

    part1 b exercises done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,7 +9,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props.parts)
+  // console.log(props.parts)
   return (
     <div>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
@@ -27,7 +27,7 @@ const Part = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
   )
 }
 
@@ -52,7 +52,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content parts={parts}/>
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Total exercises={parts} />
     </div>
   )
 }
