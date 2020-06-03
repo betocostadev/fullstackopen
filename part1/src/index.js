cbabdf75beae3f5d173fef0a18b1af2abd9452a9
betocostadev <robertomarx@gmail.com>
2020-06-03 08:41:55 -0300

    more tests to part1 c

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
     <p>
-      Hello {props.name}, you are {props.age} years old
+      Hello {name}, you are {age} years old
     </p>
+    <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
-const App = () => {
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000)
+
+  console.log('rendering...', counter)
+
+  // const handleClick = () => console.log('clicked')
+
   const now = new Date()
-  const a = 10
-  const b = 20
-
   // Using the age here and also passing the age
-  let someName = "Critos"
-  let someAge = 25
-
-  console.log('Hello from component!')
+  let someName = 'Arto Hellas'
+  let someAge = 35
 
   return (
     <div>
       <h1>Greetings!</h1>
-      <Hello name="Maya" age={26 + 10} />
       <Hello name={someName} age={someAge} />
       <p>It is now: { now.toString() }</p>
-      <p>{ a } plus { b } is: { a + b }</p>
-      <p>Is the sum correct? { a + b === 30 ? 'Yes' : 'No' }</p>
+      <p>{ counter }</p>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
     </div>
   )
 }
