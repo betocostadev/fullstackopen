b81aeb65c9b4c1703314c0c63ef1966be550433d
betocostadev <robertomarx@gmail.com>
2020-08-13 09:09:38 -0300

    small change

--- part2/learnp2/src/App.js
+++ part2/learnp2/src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Note from './components/Note'
 
 const App = ({ notes }) => {
+  const noteList = notes.map((note) => <Note key={note.id} note={note} />)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) =>
-          <Note key={note.id} note={note} />
-        )}
+        { noteList }
       </ul>
     </div>
   )
