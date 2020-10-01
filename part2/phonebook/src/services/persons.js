00b0283bb2840d5defca9fca7aea879c9399d445
betocostadev <robertomarx@gmail.com>
2020-10-01 10:24:29 -0300

    added fixes

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+// const baseUrl = 'http://localhost:3001/api/persons'
+// const baseUrl = 'https://rocky-crag-83679.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = async () => {
   try {
