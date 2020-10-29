981f542765c3e5217486e9ba7c1c93d03edebc24
betocostadev <robertomarx@gmail.com>
2020-10-29 20:02:00 -0300

    some changes to note reducer

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -8,6 +8,7 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
+      // return Object.assign({}, ...Object.keys(state).map(ky => ({[ky]: ky === 'good' ? state[ky] + 1 : state[ky]})))
       return { good: state.good + 1, ok: state.ok, bad: state.bad }
     case 'OK':
       return { good: state.good, ok: state.ok + 1, bad: state.bad }
