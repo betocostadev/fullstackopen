4bdc4d7780f7cc6de71f90a6ddcb0fc080ae29e9
betocostadev <robertomarx@gmail.com>
2021-02-10 15:31:59 -0300

    better calc

---
+++ part9/first-steps/calculator.ts
@@ -0,0 +1,56 @@
+//   We can create a type using the TypeScript native keyword type. Let's describe our type Operation:
+type Operation = 'multiply' | 'add' | 'divide';
+
+// Here we define the Result that will be used as the return of the calculator function
+// In this case we are accepting strings or numbers, since the last part of calculator can return a string
+// type Result = string | number
+
+// const calculator = (a: number, b: number, op : Operation): Result => {
+//   if (op === 'multiply') {
+//     return a * b;
+//   } else if (op === 'add') {
+//     return a + b;
+//   } else if (op === 'divide') {
+//     if (b === 0) return 'this cannot be done';
+//     return a / b;
+//   }
+// }
+
+// Making sure the calculator would work if the could receive external input
+type Result = number;
+
+const calculator = (a: number, b: number, op : Operation) : Result => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if( b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
+}
+
+console.log('calculator result 1:')
+try {
+  console.log(calculator(1, 0, 'divide'));
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
+
+console.log('calculator result 2:')
+try {
+  console.log(calculator(2000, 7, 'multiply'));
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
+
+// console.log(process.argv)
+
+// Run with npm run calculate 2 5 (2, 5 = number args)
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+const op: string = String(process.argv[4])
+console.log(a, b, op, `Multiplied ${a} and ${b}, the result is:`, calculator(a, b, op));
--- part9/first-steps/multiplier.ts
+++ part9/first-steps/multiplier.ts
@@ -1,60 +1,29 @@
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const multiplicator = (a: number, b: number, printText: string) => {
-    console.log(printText,  a * b);
-  }
-
-//   multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
-  multiplicator(10, 4, 'Multiplied two numbers, the result is:');
-
-//   We can create a type using the TypeScript native keyword type. Let's describe our type Operation:
-type Operation = 'multiply' | 'add' | 'divide';
-
-// Here we define the Result that will be used as the return of the calculator function
-// In this case we are accepting strings or numbers, since the last part of calculator can return a string
-// type Result = string | number
-
-// const calculator = (a: number, b: number, op : Operation): Result => {
-//   if (op === 'multiply') {
-//     return a * b;
-//   } else if (op === 'add') {
-//     return a + b;
-//   } else if (op === 'divide') {
-//     if (b === 0) return 'this cannot be done';
-//     return a / b;
-//   }
-// }
-
-// Making sure the calculator would work if the could receive external input
-type Result = number;
-
-const calculator = (a: number, b: number, op : Operation) : Result => {
-  switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if( b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-  }
+  console.log(printText,  a * b);
 }
 
-console.log('calculator result 1:')
 try {
-  console.log(calculator(1, 0, 'divide'));
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
 } catch (e) {
-  console.log('Something went wrong, error message: ', e.message);
+  console.log('Error, something bad happened, message: ', e.message);
 }
-
-console.log('calculator result 2:')
-try {
-  console.log(calculator(2000, 7, 'multiply'));
-} catch (e) {
-  console.log('Something went wrong, error message: ', e.message);
-}
-
-console.log(process.argv)
-
-
-
